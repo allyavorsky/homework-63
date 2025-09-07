ef2cc70ffb61e193f0e00e691d95654de3275cfa
allyavorsky <allyavorsky@proton.me>
2025-09-07 10:31:17 +0300

    HW63-2: configured express-session for user session managment

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
+require("dotenv").config();
+
 const express = require("express");
+const session = require("express-session");
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+
+    cookie: {
+      secure: false,
+      httpOnly: true,
+      maxAge: 60 * 60 * 1000,
+    },
+  })
+);
+
 app.get("/", (req, res) => {
-  res.send("<h1>Сервер Express</h1>");
+  res.send("<h1>Сервер Express. Сесії налаштовано.</h1>");
 });
 
 app.listen(PORT, () => {
