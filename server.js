06a2de9d3f7739f3b630245f2f39feb983930fda
allyavorsky <allyavorsky@proton.me>
2025-09-07 10:37:56 +0300

    HW63-3: integrated and initialized Passport.js

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 require("dotenv").config();
-
 const express = require("express");
 const session = require("express-session");
 
+const passport = require("passport");
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const users = [];
 
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-
     cookie: {
       secure: false,
       httpOnly: true,
@@ -20,8 +22,20 @@ app.use(
   })
 );
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  const user = users.find((u) => u.id === id);
+  done(null, user);
+});
+
 app.get("/", (req, res) => {
-  res.send("<h1>Сервер Express. Сесії налаштовано.</h1>");
+  res.send("<h1>Сервер Express. Passport підключено.</h1>");
 });
 
 app.listen(PORT, () => {
